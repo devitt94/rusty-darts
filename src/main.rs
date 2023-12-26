e287e805f1217d96b7193896d86f3256dea9a148
Rory Devitt <rory.devitt@wegift.io>
2023-12-26 14:39:44 +0000

    Run a comparison of aim points and resulting dart averages parameterised by dispersion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use rand::prelude::*;
 use rand_distr::Normal;
 
@@ -9,49 +10,107 @@ const TRIPLE_RING_INNER_RADIUS_MM: f64 = 99.0;
 const TRIPLE_RING_OUTER_RADIUS_MM: f64 = 107.0;
 
 
+#[derive(Clone)]
 struct DartThrow {
     x: f64,
     y: f64,
 }
 
+impl std::fmt::Display for DartThrow {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let score = compute_score(self.clone());
+        write!(f, "{}", score.representation)
+    }
+}
+
+#[derive(Eq, Hash, PartialEq)]
 struct Score {
     value: u8,
     representation: String,
 }
 
-fn main() {
 
-    let aim_bullseye = DartThrow { x: 0.0, y: 0.0 };
-    let aim_t20 = DartThrow { x: 0.0, y: 103.0 };
-    let aim_t19 = DartThrow { x: -31.83, y: -97.96};
-    let aim_t18 = DartThrow { x: 60.54, y: 83.33};
-    let aim_t17 = DartThrow { x: 31.83, y: -97.96};
-    let aim_t16 = DartThrow { x: -83.33, y: -60.54};
-    let aim_t15 = DartThrow { x: 83.33, y: -60.54};
-    let aim_t14 = DartThrow { x: -97.96, y: 31.83};
-
-    let dispersion_mm = 20.0;
-    let n_sims = 100000;
-
-    println!("Aiming at bullseye: {}", run_simulation(&aim_bullseye, dispersion_mm, n_sims));
-    println!("Aiming at T20: {}", run_simulation(&aim_t20, dispersion_mm, n_sims));
-    println!("Aiming at T19: {}", run_simulation(&aim_t19, dispersion_mm, n_sims));
-    println!("Aiming at T18: {}", run_simulation(&aim_t18, dispersion_mm, n_sims));
-    println!("Aiming at T17: {}", run_simulation(&aim_t17, dispersion_mm, n_sims));
-    println!("Aiming at T16: {}", run_simulation(&aim_t16, dispersion_mm, n_sims));
-    println!("Aiming at T15: {}", run_simulation(&aim_t15, dispersion_mm, n_sims));
-    println!("Aiming at T14: {}", run_simulation(&aim_t14, dispersion_mm, n_sims));
-
+struct SimulationInput {
+    aim: DartThrow,
+    dispersion_mm: f64,
+    n_sims: i32,
 }
 
-fn run_simulation(aim: &DartThrow, dispersion_mm: f64, n_sims: i32) -> f64 {
+struct SimulationResult {
+    average_score: f64,
+    score_counts: HashMap<Score, i32>,
+}
+
+impl std::fmt::Display for SimulationResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Average score: {}", self.average_score)
+    }
+}
+
+fn main() {
+
+    let dispersion_range: Vec<f64> = (0..=20).map(|x| x as f64).map(|x| (x * 2.5) + 5.0).collect();
+
+    let n_sims = 100000;
+    for dispersion_mm in dispersion_range {
+        let sim_results = run_comparison_simulations(dispersion_mm, n_sims);
+        println!("Dispersion: {}mm", dispersion_mm);
+        println!("Best to worst average scores:");
+        for (sim_input, sim_result) in sim_results.iter() {
+            println!("\t{}: {}", sim_input.aim, sim_result.average_score);
+        }
+    }
+}
+
+fn run_comparison_simulations(dispersion_mm: f64, n_sims: i32) -> Vec<(SimulationInput, SimulationResult)> {
+
+    let aim_points: HashMap<String, DartThrow> = HashMap::from([
+        ("bullseye".to_string(), DartThrow { x: 0.0, y: 0.0 }),
+        ("t20".to_string(), DartThrow { x: 0.0, y: 103.0 }),
+        ("t19".to_string(), DartThrow { x: -31.83, y: -97.96}),
+        ("t18".to_string(), DartThrow { x: 60.54, y: 83.33}),
+        ("t17".to_string(), DartThrow { x: 31.83, y: -97.96}),
+        ("t16".to_string(), DartThrow { x: -83.33, y: -60.54}),
+        ("t15".to_string(), DartThrow { x: 83.33, y: -60.54}),
+        ("t14".to_string(), DartThrow { x: -97.96, y: 31.83}),
+    
+    ]);
+
+    let mut simulations: Vec<(SimulationInput, SimulationResult)> = Vec::new();
+    
+    for (_aim_name, aim) in aim_points.into_iter() {
+        let sim_input = SimulationInput {
+            aim: aim.clone(),
+            dispersion_mm,
+            n_sims,
+        };
+        let result = run_simulation(&sim_input);
+        simulations.push((sim_input, result));
+    }
+
+    simulations.sort_by(|a, b| b.1.average_score.partial_cmp(&a.1.average_score).unwrap());
+    simulations
+}
+
+fn run_simulation(sim_input: &SimulationInput) -> SimulationResult {
     let mut total_score: i32 = 0;
-    for _i in 0..n_sims {
-        let throw = throw_dart(dispersion_mm, aim);
+    let mut score_counts: HashMap<Score, i32> = HashMap::new();
+    for _i in 0..sim_input.n_sims {
+        let throw = throw_dart(sim_input.dispersion_mm, &sim_input.aim);
         let score = compute_score(throw);
         total_score += score.value as i32;
+        if score_counts.contains_key(&score) {
+            let count = score_counts.get_mut(&score).unwrap();
+            *count += 1;
+        } else {
+            score_counts.insert(score, 1);
+        }
     }
-    f64::from(total_score) / n_sims as f64
+    let average_score: f64 = f64::from(total_score) / sim_input.n_sims as f64;
+    SimulationResult {
+        average_score,
+        score_counts,
+    }
 }
 
 
